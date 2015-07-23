872315b43f94423b4b645fe75f7f10fb832f4645
Bradley Clemetson <brad.clemetson@codeprogrammers.net>
2015-07-22 21:31:09 -0700

    Added Apple starting project

---
+++ MetalSamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MetalSamples
+//
+//  Created by Bradley Clemetson on 7/22/15.
+//  Copyright (c) 2015 Sample. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ MetalSamples/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  MetalSamples
+//
+//  Created by Bradley Clemetson on 7/22/15.
+//  Copyright (c) 2015 Sample. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <MetalKit/MTKView.h>
+
+@interface GameViewController : NSViewController<MTKViewDelegate>
+
+
+@end
